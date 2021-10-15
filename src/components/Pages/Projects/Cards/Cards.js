904bacde5a8e25126961f35b3253927b755f1bc5
unknown <alicee5cha@gmail.com>
2021-10-15 23:46:10 +0100

    fixed navbar weirdness

--- src/components/Pages/Projects/Cards/Cards.js
+++ src/components/Pages/Projects/Cards/Cards.js
@@ -1,19 +1,12 @@
 import React, { Component } from 'react';
 import CardItem from './CardItem';
 import './Cards.scss';
-import placeholder from '../../../../images/img-1.jpg';
 //in a database preferably
 
 export default class Cards extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      src: '',
-      text: '',
-      language: '',
-      path: '',
-      colourCode: ''
-    };
+    this.state = {};
   }
 
   render() {
@@ -21,43 +14,13 @@ export default class Cards extends Component {
       <>
         <div className="cards">
           <div className="cards-items">
-            <CardItem
-              src={placeholder}
-              text="CSS used to create a stunning contact form"
-              language="CSS"
-              path="/project1"
-              colourCode="#1f98f4"
-            />
-            <CardItem
-              src={placeholder}
-              text="CSS used to create a stunning contact form"
-              language="CSS"
-              path="/project1"
-              colourCode="#1f98f4"
-            />
+            <CardItem />
+            <CardItem />
           </div>
           <div className="cards-items">
-            <CardItem
-              src={placeholder}
-              text="CSS used to create a stunning contact form"
-              language="CSS"
-              path="/project1"
-              colourCode="#1f98f4"
-            />
-            <CardItem
-              src={placeholder}
-              text="CSS used to create a stunning contact form"
-              language="CSS"
-              path="/project1"
-              colourCode="#1f98f4"
-            />
-            <CardItem
-              src={placeholder}
-              text="CSS used to create a stunning contact form"
-              language="CSS"
-              path="/project1"
-              colourCode="#1f98f4"
-            />
+            <CardItem />
+            <CardItem />
+            <CardItem />
           </div>
         </div>
       </>
