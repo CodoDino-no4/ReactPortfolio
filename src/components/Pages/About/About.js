a866600118aeba70d43d3bd8908628926bad9d70
Alice Chambers <alicee5cha@gmail.com>
2021-12-22 17:30:52 +0000

    start of about section

--- src/components/Pages/About/About.js
+++ src/components/Pages/About/About.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './About.scss';
 
 export default class About extends Component {
   constructor(props) {
@@ -10,6 +11,9 @@ export default class About extends Component {
     return (
       <section className="about">
         <h1>ABOUT</h1>
+        <div className="inner-rect">
+          <div className="circle-image-me"></div>
+        </div>
       </section>
     );
   }
