2318bb55f6c17c51f5cad041a13551725394e1dc
Alice Chambers <alicee5cha@gmail.com>
2022-05-19 20:00:55 +0100

    footer with artwork

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,25 +1,31 @@
 import React from 'react';
 import './Footer.scss';
+import FooterArt from './SVG/FooterArt';
 
 const Footer = () => (
   <>
     <div className="footer">
-      <div className="links-grid">
-        <div className="grid-item">
-          <i className="fab fa-linkedin"></i>
+      <div className="footer-items">
+        <div className="links-grid">
+          <div className="grid-item">
+            <i className="fab fa-github"></i>GitHub
+          </div>
+          <div className="grid-item">
+            <i className="fab fa-linkedin"></i>LinkedIn
+          </div>
+          <div className="grid-item">
+            <i className="fas fa-code"></i>FrontEnd Mentor
+          </div>
+          <div className="grid-item">
+            <i className="fas fa-envelope"></i>Email
+          </div>
         </div>
-        <div className="grid-item">
-          <i className="fab fa-github"></i>
-        </div>
-        <div className="grid-item">
-          <i className="fas fa-code"></i>
-        </div>
-        <div className="grid-item">
-          <i className="fas fa-envelope"></i>
+        <div className="footer-art">
+          <FooterArt />
         </div>
       </div>
       <div className="footer-br">
-        <p>Copyright Alice Chambers</p>
+        <p>&copy; Copyright Alice Chambers</p>
       </div>
     </div>
   </>
