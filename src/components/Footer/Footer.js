42824efc910b1591d29899fa0dbf92eff370f5b1
Alice Chambers <alicee5cha@gmail.com>
2022-05-20 19:45:05 +0100

    better icons and layout

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import './Footer.scss';
 import FooterArt from './SVG/FooterArt';
+import linkedin from '../../images/linkedin-logo.svg';
+import github from '../../images/github-logo.svg';
+import code from '../../images/code-logo.svg';
+import email from '../../images/envelope-logo.svg';
 
 const Footer = () => (
   <>
@@ -8,16 +12,20 @@ const Footer = () => (
       <div className="footer-items">
         <div className="links-grid">
           <div className="grid-item">
-            <i className="fab fa-github"></i>GitHub
+            <img src={github} />
+            <p>GitHub</p>
           </div>
           <div className="grid-item">
-            <i className="fab fa-linkedin"></i>LinkedIn
+            <img src={linkedin} />
+            <p>LinkedIn</p>
           </div>
           <div className="grid-item">
-            <i className="fas fa-code"></i>FrontEnd Mentor
+            <img src={code} />
+            <p>FrontEnd Mentor</p>
           </div>
           <div className="grid-item">
-            <i className="fas fa-envelope"></i>Email
+            <img src={email} />
+            <p>Email</p>
           </div>
         </div>
         <div className="footer-art">
