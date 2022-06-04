38fd2eb426eaadd4224081f381027a975fe5de32
Alice Chambers <alicee5cha@gmail.com>
2022-06-04 22:23:04 +0100

    some general UI standardisation

--- src/components/Sections/Contact/Contact.js
+++ src/components/Sections/Contact/Contact.js
@@ -12,39 +12,37 @@ export default class Contact extends Component {
       <section className="contact">
         <h1>CONTACT</h1>
         <div className="container">
-          <div className="contactForm">
-            <div className="headings">
-              <h2>Lets connect...</h2>
-            </div>
-            <form onSubmit={this.handleSubmit}>
-              <div className="contactColumn no1">
-                <div className="row">
-                  <input type="text" required />
-                  <span>Name</span>
-                </div>
-                <div className="row">
-                  <input type="text" required />
-                  <span>Email</span>
-                </div>
-                <div className="row">
-                  <input type="text" required />
-                  <span>Company</span>
-                </div>
-                <div className="row">
-                  <input type="text" required />
-                  <span>Location</span>
-                </div>
-              </div>
-              <div className="contactColumn no2">
-                <div className="row">
-                  <textarea required></textarea>
-                  <span>Write your message here...</span>
-                </div>
-                <div className="textAreaRow"></div>
-              </div>
-              <input type="submit" value="Send" />
-            </form>
+          <div className="headings">
+            <h2>Lets connect...</h2>
           </div>
+          <form onSubmit={this.handleSubmit}>
+            <div className="contactColumn no1">
+              <div className="row">
+                <input type="text" required />
+                <span>Name</span>
+              </div>
+              <div className="row">
+                <input type="text" required />
+                <span>Email</span>
+              </div>
+              <div className="row">
+                <input type="text" required />
+                <span>Company</span>
+              </div>
+              <div className="row">
+                <input type="text" required />
+                <span>Location</span>
+              </div>
+            </div>
+            <div className="contactColumn no2">
+              <div className="row">
+                <textarea required></textarea>
+                <span>Write your message here...</span>
+              </div>
+              <div className="textAreaRow"></div>
+            </div>
+            <input type="submit" value="Send" />
+          </form>
         </div>
       </section>
     );
