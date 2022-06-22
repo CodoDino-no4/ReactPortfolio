fa09c4cad4f96f4525094cc70621b7ccf1697b3e
Alice Chambers <alicee5cha@gmail.com>
2022-06-22 19:00:32 +0100

    actually fixed webpack yaya

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,7 @@
 const path = require('path');
-const webpack = require('webpack');
 
 module.exports = {
-  entry: ['react-hot-loader/patch', './src/index.js'],
+  entry: ['./src/index.js'],
   mode: 'development',
   module: {
     rules: [
@@ -24,15 +23,12 @@ module.exports = {
   },
   resolve: { extensions: ['*', '.js', '.jsx'] },
   output: {
-    path: path.resolve(__dirname, 'dist/'),
     publicPath: '/dist/',
+    path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
   },
   devServer: {
-    contentBase: path.join(__dirname, 'public/'),
     port: 3000,
-    publicPath: 'http://localhost:3000/dist/',
-    hotOnly: true
-  },
-  plugins: [new webpack.HotModuleReplacementPlugin()]
+    hot: true
+  }
 };
