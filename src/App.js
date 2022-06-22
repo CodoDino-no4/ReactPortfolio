3612ccd0aeacd3e96d9663b17cb2f2b0bca8f9e5
Alice Chambers <alicee5cha@gmail.com>
2022-06-23 00:19:18 +0100

    started on redirect links

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { hot } from 'react-hot-loader/root';
 import React, { Component } from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.scss';
 import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
@@ -8,21 +8,23 @@ import Home from './components/Sections/Home/Home';
 import About from './components/Sections/About/About';
 import Projects from './components/Sections/Projects/Projects';
 import Contact from './components/Sections/Contact/Contact';
+import RedirectPage from './components/Redirect/Redirect';
 // import NotFound from './components/Pages/404/NotFound';
 
 class App extends Component {
   render() {
     return (
-      <>
-        <Router>
-          <Navbar />
-          <Home />
-          <About />
-          <Projects />
-          <Contact />
-          <Footer />
-        </Router>
-      </>
+      <Router>
+        <Navbar />
+        <Home />
+        <About />
+        <Projects />
+        <Contact />
+        <Footer />
+        <Routes>
+          <Route path="/external-link" element={<RedirectPage />} />
+        </Routes>
+      </Router>
     );
   }
 }
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,44 +1,61 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Footer.scss';
 import FooterArt from './SVG/FooterArt';
 import linkedin from '../../images/linkedin-logo.svg';
 import github from '../../images/github-logo.svg';
 import code from '../../images/code-logo.svg';
 import email from '../../images/envelope-logo.svg';
-import Link from 'react-scroll/modules/components/Link';
 
 const Footer = () => (
-  <>
-    <div className="footer">
-      <div className="footer-items">
-        <div className="links-grid">
-          <Link className="grid-item" to={'https://github.com/Alicee5cha'}>
-            <img src={github} />
-            <p>GitHub</p>
-          </Link>
-          <Link className="grid-item">
-            <img src={linkedin} />
-            <p>LinkedIn</p>
-          </Link>
-          <Link className="grid-item">
-            <img src={code} />
-            <p>FrontEnd Mentor</p>
-          </Link>
-          <Link className="grid-item">
-            <img src={email} />
-            <p>Email</p>
-          </Link>
-        </div>
-        <div className="footer-art">
-          <FooterArt />
-        </div>
+  <div className="footer">
+    <div className="footer-items">
+      <div className="links-grid">
+        <Link
+          className="grid-item"
+          to="/external-link"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={github} />
+          <p>GitHub</p>
+        </Link>
+        <Link
+          className="grid-item"
+          to="/external-link"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={linkedin} />
+          <p>LinkedIn</p>
+        </Link>
+        <Link
+          className="grid-item"
+          to="/external-link"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={code} />
+          <p>FrontEnd Mentor</p>
+        </Link>
+        <Link
+          className="grid-item"
+          to="/external-link"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={email} />
+          <p>Email</p>
+        </Link>
       </div>
-      <div className="footer-br">
-        <p>Designed by Alice Chambers</p>
-        <p>&copy; Copyright Alice Chambers 2022</p>
+      <div className="footer-art">
+        <FooterArt />
       </div>
     </div>
-  </>
+    <div className="footer-br">
+      <p>Designed by Alice Chambers</p>
+    </div>
+  </div>
 );
 
 export default Footer;
