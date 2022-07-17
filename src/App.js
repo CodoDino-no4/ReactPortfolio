feca9db9cf8efd1abb512b3a3ccf10d2a551a7f0
Alice Chambers <alicee5cha@gmail.com>
2022-07-17 19:35:43 +0100

    small changes to @media and started about section layout

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Home from './components/Sections/Home/Home';
 import About from './components/Sections/About/About';
 import Projects from './components/Sections/Projects/Projects';
 import Contact from './components/Sections/Contact/Contact';
-import RedirectPage from './components/Redirect/Redirect';
+import RedirectPage from './components/Pages/Redirect/Redirect';
 // import NotFound from './components/Pages/404/NotFound';
 
 function App() {
