a5925ad2f667577c6e48cd95de52eee11944b22e
Alice Chambers <alicee5cha@gmail.com>
2022-07-18 16:48:57 +0100

    footer container padding fixed

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -9,51 +9,53 @@ import email from '../../images/envelope-logo.svg';
 
 const Footer = () => (
   <div className="footer">
-    <div className="footer-items">
-      <div className="links-grid">
-        <Link
-          className="grid-item"
-          to="/external-link"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img src={github} />
-          <p>GitHub</p>
-        </Link>
-        <Link
-          className="grid-item"
-          to="/external-link"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img src={linkedin} />
-          <p>LinkedIn</p>
-        </Link>
-        <Link
-          className="grid-item"
-          to="/external-link"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img src={code} />
-          <p>FrontEnd Mentor</p>
-        </Link>
-        <Link
-          className="grid-item"
-          to="/external-link"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img src={email} />
-          <p>Email</p>
-        </Link>
+    <div className="footer-container">
+      <div className="footer-items">
+        <div className="links-grid">
+          <Link
+            className="grid-item"
+            to="/external-link"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={github} />
+            <p>GitHub</p>
+          </Link>
+          <Link
+            className="grid-item"
+            to="/external-link"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={linkedin} />
+            <p>LinkedIn</p>
+          </Link>
+          <Link
+            className="grid-item"
+            to="/external-link"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={code} />
+            <p>FrontEnd Mentor</p>
+          </Link>
+          <Link
+            className="grid-item"
+            to="/external-link"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={email} />
+            <p>Email</p>
+          </Link>
+        </div>
+        <div className="footer-art">
+          <FooterArt />
+        </div>
       </div>
-      <div className="footer-art">
-        <FooterArt />
+      <div className="footer-br">
+        <p>Designed by Alice Chambers</p>
       </div>
-    </div>
-    <div className="footer-br">
-      <p>Designed by Alice Chambers</p>
     </div>
   </div>
 );
